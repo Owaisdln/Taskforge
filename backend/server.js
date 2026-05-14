78d350653d946fac041c1b1d6d9792abe23e762a
Owaisdln <owaisdln0786@gmail.com>
2026-05-14 14:06:17 +0530

    Fix Express 5 wildcard routing error

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   // Any route that is not an API route will be redirected to index.html
-  app.get("*", (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 } else {
